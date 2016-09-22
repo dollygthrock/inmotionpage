96b9b82ebba48cfbd410a0e8e2c43f998330afba
dollygthrock <dollygthrock@gmail.com>
2016-09-22 08:43:18 -0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+ruby '2.0.0'
+
+gem 'sinatra', '~>1.4.7'
+
+gem 'pony', '~>1.11'
+
---
+++ app.rb
@@ -0,0 +1,20 @@
+require "sinatra"
+require "pony"
+
+load "./local_env.rb" if File.exist?("./local_env.rb")
+
+get '/' do 
+	erb :index
+end
+
+get '/blog' do
+	erb :blog
+end
+
+get '/contact' do
+	erb :contact
+end
+
+get '/about' do
+	erb :about
+end 
